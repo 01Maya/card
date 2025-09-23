187ff0de891be3849c61986d8e150765ee5f97a1
Mansi_Patel <patelmaya9812@gmail.com>
2025-09-24 01:07:25 +0530

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -22,7 +22,8 @@ export function Hero() {
             transition={{ duration: 0.6 }}
             className="text-center md:text-left"
           >
-            <h1 className="text-4xl md:text-5xl font-bold text-white mb-6">Card &quot;Next&quot;</h1>
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
+            <h1 className="text-4xl md:text-5xl font-bold text-white mb-6">Card "Next"</h1>
             <p className="text-lg text-white/80 mb-8 max-w-md mx-auto md:mx-0">
               Your personal card for daily purposes. This card was created to help you make wisely purchases and get
               benefits with joy. Discover all advantages and get your card today!
